db0485f859d99a0d181501911e74e5b2a6007780
Raghu Ranganathan <rraghu.11502@gmail.com>
2021-04-24 10:20:42 +0000

    Added all commands

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def grid():
   return "```\n"+"\n".join(["".join(i) for i in board])+ "\n```"
 
 
-@bot.command(name='play', help='Starts a game with another person, given board size.')
+@bot.command(name='play', help='[player1 player2 width height]: Starts a game with another person, given board size.')
 async def play(ctx,pl1:discord.Member,pl2:discord.Member,w:int,h:int):
   global p1
   global p2
@@ -50,13 +50,15 @@ async def play(ctx,pl1:discord.Member,pl2:discord.Member,w:int,h:int):
     else:
       turn = p2
     pieces = random.sample(cmds,k=4)
+    await ctx.send("<@" + str(p1.id) + "> -> A")
+    await ctx.send("<@" + str(p2.id) + "> -> B")
     await ctx.send("<@" + str(turn.id) + ">'s turn") 
     await ctx.send("Pieces: `"+",".join(pieces)+"`")
   else:
     await ctx.send("A game is in progress. Please wait.")
 
 
-@bot.command(name='move', help='places char at the given coordinates.')
+@bot.command(name='move', help='[char x y]: places char at the given coordinates.')
 async def move(ctx,char,x:int,y:int):
   global p1
   global p2
@@ -69,7 +71,7 @@ async def move(ctx,char,x:int,y:int):
   else:
     if turn == ctx.author:
       if(abs(y)<len(board) and abs(x)<len(board[0]) and char in pieces):
-        board[y][x] == char
+        board[y][x] = char
         await ctx.send("Current Board:\n"+grid())
       else:
         await ctx.send("Invalid move. Try again.")
@@ -78,10 +80,84 @@ async def move(ctx,char,x:int,y:int):
 
 @bot.command(name='execute', help='executes the board and determines the result. Ends the game as well.')
 async def execute(ctx):
-  await ctx.send("Executing")
+  global board
+  global gameOver
+  if(gameOver):
+    ctx.send("There is no running board to execute.")
+    return
+  if turn == ctx.author:
+    r,c=0,0
+    dr,dc=0,1
+    stack = []
+    curr = board[r][c]
+    route = []
+    stringMode = False
+    string = ""
+    while(1):
+      curr = board[r][c]
+      if([r,c] in route):
+        ctx.send("Tie")
+        gameOver = True
+        return
+      elif(stringMode and curr != '"'):
+        string += curr
+      elif(curr == 'A'):
+        ctx.send("<@" +str(p1.id) + "> wins")
+        gameOver = True
+        return
+      elif(curr == 'B'):
+        ctx.send("<@" +str(p2.id) + "> wins")
+        gameOver = True
+        return
+      elif(curr == '>'):
+        dr,dc = 0,1
+      elif(curr == '<'):
+        dr,dc = 0,-1
+      elif(curr == '^'):
+        dr,dc = -1,0
+      elif(curr == 'v'):
+        dr,dc = 1,0
+      elif(curr in '0123456789'):
+        stack.append(int(curr))
+      elif(curr in '+-%/*'):
+        stack.append(eval(str(stack.pop()) + curr + str(stack.pop())))
+      elif(curr == '?'):
+        dr = random.randint(-1,1)
+        dc = random.randint(-1,1)
+      elif(curr == '#'):
+        r += dr
+        c += dc
+      elif(curr == '"'):
+        stringMode = not stringMode
+        string = ""
+      elif(curr == '@'):
+        r += 2*dr
+        c += 2*dc
+      elif(curr == '|'):
+        if(stack.pop()):
+          dr = -1
+          dc = 0
+        else:
+          dr = 1
+          dc = 0
+      elif(curr == '_'):
+          if(stack.pop()):
+            dr = 0
+            dc = -1
+          else:
+            dr = 0
+            dc = 1
+      else:
+        pass
+      route.append([r,c])
+      r += dr
+      c += dc
+  else:
+    await ctx.send("You can execute the board on your turn.")
 
 @bot.command(name='end', help='ends the game without deciding a result.')
-def endGame(ctx):
+async def endGame(ctx):
+  global gameOver
   if(gameOver):
     await ctx.send("There is no game to end.")
   else:
